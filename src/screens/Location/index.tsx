ad32e3b0b5f8ed33ab08b051557e5a669c06083c
mariaeduarda2504 <mariaeduardad391@gmail.com>
2023-07-03 18:34:09 -0300

    Terminado

--- src/screens/Location/index.tsx
+++ src/screens/Location/index.tsx
@@ -1,83 +1,145 @@
 import React, { useState, useEffect, useRef } from 'react';
-import {API_GOOGLE} from '@env';
 import { Text, View } from 'react-native';
 import * as Location from 'expo-location';
-import MapView, {Region, Marker, Polyline} from 'react-native-maps';
+import MapView, { Region, Marker, Polyline } from "react-native-maps"
 import { styles } from "./styles"
 import { colors } from '../../styles/colors';
-import {GooglePlacesAutocomplete, GooglePlaceData, GooglePlaceDetail} from 'react-native-google-places-autocomplete';
-import MapViewDirections from 'react-native-maps-directions'
+import { API_GOOGLE }  from '@env';
+import { GooglePlaceData, GooglePlaceDetail, GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import MapViewDirections from 'react-native-maps-directions';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export function LocationScreen() {
-  const [location, setLocation] = useState<Location.LocationObject | null>(null);
-  const [errorMsg, setErrorMsg] = useState<string | null>(null);
-  const [region, setRegion] = useState<Region>()
-  const [marker, setMarker] = useState<Region[]>()
-  const [coords, setCoords] = useState<ICoords []>([])
-  const [destination, setDestination] = useState<Region | null>(null)
-  const mapRef = useRef<MapView>(null)
-
-  type ICoords = {
+type ICoords ={
     latitude: number
     longitude: number
-  }
+}
 
-  useEffect(() => {
-    let subscription: Location.LocationSubscription
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('A permissão para acessar sua localização foi negada');
-        return;
-      }
+export function LocationScreen() {
+    const [location, setLocation] = useState<Location.LocationObject | null>(null);
+    const [errorMsg, setErrorMsg] = useState<string | null>(null);
+    const [region, setRegion] = useState<Region>()
+    const [marker, setMarker] = useState<Region[]>()
+    const [coords, setCoords] = useState<ICoords[]>([])
+    const [destination, setDestination] = useState<Region | null>(null)
+    const mapRef = useRef<MapView>(null)
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-      setRegion({
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.006,
-        longitudeDelta: 0.006
-    })
-    setMarker([{
-        latitude: location.coords.latitude,
-        longitude: location.coords.longitude,
-        latitudeDelta: 0.006,
-        longitudeDelta: 0.006
-    }])
-    subscription = await Location.watchPositionAsync({
-      accuracy: Location.LocationAccuracy.High,
-      timeInterval: 1000,
-      distanceInterval: 1
-    }, (location) => {
-      setCoords((prevState)=> [...prevState, location.coords])
-    });
-    })();
-  }, []);
+    useEffect(() => {
+        let subscription: Location.LocationSubscription
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('A permissão para acessar a localização foi negada');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+            setRegion({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.009,
+                longitudeDelta: 0.009
+            })
+            setMarker([{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004
+            }])
+            subscription = await Location.watchPositionAsync({
+                accuracy: Location.LocationAccuracy.High,
+                timeInterval: 1000,
+                distanceInterval: 1
+            }, (location) => {
+                setCoords((prevState) => [...prevState, location.coords])
+            });
+        })();
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+        return () => {
+            if (subscription) {
+                subscription.remove()
+            }
+        }
+    }, []);
 
-  return (
-    <View style={styles.container}>
-        {region ? (
-            <MapView region={region} style={styles.map} >
-            {marker && marker.map((i)=>(
-                <Marker key={i.latitude} coordinate={i}/>
-            ))}
-            {coords && <Polyline
-              coordinates={coords}
-              strokeColor={colors.black}
-              strokeWidth={7}
-            />}
-            </MapView>
-        ):(
-            <Text style={styles.paragraph}>{text}</Text>
-        )}
-    </View>
-  );
+    async function handleDestination(data: GooglePlaceData, details: GooglePlaceDetail | null) {
+        if (details) {
+            setDestination({
+                latitude: details?.geometry.location.lat,
+                longitude: details?.geometry.location.lng,
+                latitudeDelta: 0.004,
+                longitudeDelta: 0.004
+            })
+            if (marker) {
+                setMarker([...marker, {
+                    latitude: details?.geometry.location.lat,
+                    longitude: details?.geometry.location.lng,
+                    latitudeDelta: 0.004,
+                    longitudeDelta: 0.004
+                }])
+            }
+        }
+    }
+    
+    let text = 'Waiting..';
+    if (errorMsg) {
+        text = errorMsg;
+    } else if (location) {
+        text = JSON.stringify(location);
+    } else if (destination) {
+        text = JSON.stringify(destination);
+    }
+
+    return (
+        <View style={styles.container}>
+             <GooglePlacesAutocomplete
+                styles={{ container: styles.searchContainer, textInput: styles.searchInput }}
+                placeholder="Destino"
+                fetchDetails={true}
+                GooglePlacesDetailsQuery={{ fields: "geometry"}}
+                enablePoweredByContainer={false}
+                query={{
+                    key: API_GOOGLE,
+                    language: 'pt-BR'
+                }}
+                onFail={setErrorMsg}
+                onPress={handleDestination}
+            />
+            {region ? (
+                <MapView ref={mapRef} region={region} style={styles.map} showsUserLocation={true}>
+                    {marker && marker.map((i) => (
+                    <Marker key = {i.latitude} coordinate = {i}>
+                        <MaterialCommunityIcons name="cellphone-marker" size={48} color={colors.primary} />
+                    </Marker>
+                    ))}
+                    {coords && <Polyline
+                        coordinates={coords}
+                        strokeColor={colors.black}
+                        strokeWidth={7}
+                    />}
+                    {destination && (
+                        <MapViewDirections
+                            origin={region}
+                            destination={destination}
+                            apikey={API_GOOGLE}
+                            strokeColor={colors.black}
+                            strokeWidth={7}
+                            lineDashPattern={[0]}
+                            onReady={(result) =>{
+                                mapRef.current?.fitToCoordinates(result.coordinates, {
+                                    edgePadding: {
+                                        top: 24,
+                                        bottom: 24,
+                                        left: 24,
+                                        right: 24
+                                    }
+                                })
+                            }}
+                        />
+                    )}
+                </MapView>    
+           ) : (
+                <Text style={styles.paragraph}>{text}</Text>
+            )}
+        </View>
+    );
 }
--- src/screens/Location/styles.ts
+++ src/screens/Location/styles.ts
@@ -1,4 +1,5 @@
-import {Dimensions, StyleSheet} from 'react-native'
+import { Dimensions, StyleSheet } from "react-native";
+import { colors } from "../../styles/colors";
 
 export const styles = StyleSheet.create({
     container: {
@@ -11,8 +12,20 @@ export const styles = StyleSheet.create({
       fontSize: 18,
       textAlign: 'center',
     },
-    map:{
+    map: {
         width: Dimensions.get("window").width,
         height: Dimensions.get("window").height
+    },
+    searchContainer: {
+      position: 'absolute',
+      zIndex: 1,
+      width: '90%',
+      top: 10,
+    },
+    searchInput : {
+      height: 56,
+      borderRadius: 7,
+      borderWidth: 2,
+      borderColor: colors.secondary
     }
-  });
+});
